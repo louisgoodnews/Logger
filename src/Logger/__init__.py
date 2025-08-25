896ec89902e6151f60c0589abf95df6c2f48d985
Louis Goodnews <louisgoodnews95@gmail.com>
2025-08-25 09:32:43 +0200

    Updated a few things.

--- src/Logger/__init__.py
+++ src/Logger/__init__.py
@@ -1,5 +1,12 @@
-from typing import Final, List
+"""
+Author: Louis Goodnews
+Date: 2025-08-25
+"""
+
+from typing import Final, List, Literal
 
 from .core.core import Logger, Level
 
 __all__: Final[List[str]] = ["Logger", "Level"]
+
+__version__: Final[Literal["0.1.0"]] = "0.1.0"
